e20df47cd3e934fc3585883e7503b76e2b91c281
Basavaraj <basavarj7065@gmail.com>
2026-09-04 16:28:05 +0530

    CICD Test 2

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo{
     public static void main(String[] args) {
         System.out.println("Hello From Demo");
-        
+        System.out.println("Hello From Demo CI CD");
     }
 }
