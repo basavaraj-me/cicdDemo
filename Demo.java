20da5743982b2d9a4c6871bfd06c01250fc7213c
Basavaraj <basavarj7065@gmail.com>
2026-09-04 16:33:58 +0530

    Add empty line for better readability

--- Demo.java
+++ Demo.java
@@ -2,6 +2,7 @@ public class Demo{
     public static void main(String[] args) {
         System.out.println("Hello From Demo");
         System.out.println("Hello From Demo CI CD");
+
         
     }
 }
